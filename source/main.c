e49c0ea005ab53376300a6311f62bed4f64c656b
ShamanUnderwear <charleston_rh@msn.com>
2026-09-02 14:50:03 -0400

    Filesystem: .nds filter and directory display

--- source/main.c
+++ source/main.c
@@ -22,16 +22,18 @@
 //Variables
 char* items[MAX_ITEMS]; //static array for printing dump from SD
 char  item_storage[MAX_ITEMS][256]; //character storage for readdir buffer, 32 items, each with a 256 byte buffer
+char item_type[MAX_ITEMS]; //array to test if entry is a directory
 //int itemcount = sizeof(items)/sizeof(items[0]); 
 int itemcount = 0;
 //Functions
 void drawMenu(int selection) {
 		//clears screen
 		iprintf("\x1b[2J");
-		//prints list and conditional selector print based on state
+		//prints list, conditional selector print based on state, and directory signifier prefix
 		for (int i = 0 ; i < itemcount; i++) {
-			const char* prefix = (i == selection) ? "-->" : "   ";
-			iprintf("\x1b[%d;1H %s %s", 5 + i, prefix, items[i]);
+			const char* prefix = (i == selection) ? ">" : " ";
+			const char* dprefix = (item_type[i] == DT_DIR) ? "/" : " ";
+			iprintf("\x1b[%d;1H %s %s %s", 5 + i, prefix, dprefix, items[i]);
 		}
 	}
 //---------------------------------------------------------------------------------
@@ -40,27 +42,53 @@ int main(int argc, char* argv[]) {
 
 	consoleDemoInit();
 	if (fatInitDefault()) {
-		iprintf("\x1b[8;1H FAT loads");
+		iprintf("\x1b[3;2H FAT loads");
 	}else{
-		iprintf("\x1b[8;1H FAT fails");
+		iprintf("\x1b[3;2H FAT fails");
 	}
 	struct dirent * entry; // pointer for each entry in filesystem
 	DIR * dir; //pointer for directory
 	int selection = 0; //selector state variable
 	//char buffer[256]; //buffer variable from testing getcwd()
 	dir = opendir(".");
-	iprintf("\x1b[9;1H %p", dir);
-	if (dir != NULL){
+	//iprintf("\x1b[4;1H %p", dir); diagnostic line, shows if opendir is returning null or not
+	if (dir != NULL ){
 		while ((entry = readdir(dir)) != NULL && itemcount < MAX_ITEMS){
-		strncpy(item_storage[itemcount], entry->d_name, 255); //readdir returns pointer to buffer, strncpy used to copy bytes(characters) from buffer to persistent storage
-		item_storage[itemcount][255] = '\0'; //adds null byte to end of string to guarantee it is terminated, preventing over read (Heartbleed reference)
-		items[itemcount] = item_storage[itemcount]; //appends string to array for SD dump
-		itemcount ++;
+			if (entry->d_name[0] == '.') 
+				continue;
+			if (entry->d_type == DT_DIR){
+				item_type[itemcount] = entry->d_type;
+				strncpy(item_storage[itemcount], entry->d_name, 255); //readdir returns pointer to buffer, strncpy used to copy bytes(characters) from buffer to persistent storage
+				item_storage[itemcount][255] = '\0'; //adds null byte to end of string to guarantee it is terminated, preventing over read (Heartbleed reference)
+				items[itemcount] = item_storage[itemcount]; //appends string to array for SD dump
+				itemcount++;
+				continue;
+			}
+			char *dot_loc;
+			dot_loc = strrchr(entry->d_name, '.');
+			if (dot_loc != NULL){
+				if (strcasecmp(dot_loc, ".nds") != 0)
+					continue;
+			}
+			else{
+				continue;
+			}
+			item_type[itemcount] = entry->d_type;
+			strncpy(item_storage[itemcount], entry->d_name, 255); //readdir returns pointer to buffer, strncpy used to copy bytes(characters) from buffer to persistent storage
+			item_storage[itemcount][255] = '\0'; //adds null byte to end of string to guarantee it is terminated, preventing over read (Heartbleed reference)
+			items[itemcount] = item_storage[itemcount]; //appends string to array for SD dump
+			itemcount ++;
 		}
+		if (readdir(dir) != NULL){
+			iprintf("\x1b[3;1H output truncated by display limit");
+		}
+		closedir(dir);
 	}else{
-		iprintf("\x1b[10;1H error: null pointer from open directory operation");
+		iprintf("\x1b[3;1H error: null pointer from open directory operation");
 	}
-	closedir(dir);
+	if (itemcount == 0){
+		iprintf("\x1b[4;2H No files found"); //when items is empty, prevents potential crashes and prints helpful message
+	}
 	drawMenu(selection); //initial menu draw
 	
 
@@ -69,7 +97,7 @@ int main(int argc, char* argv[]) {
 		scanKeys();
 		int pressed = keysDown();
 		//Scroll up, wraps to bottom
-		if(pressed & KEY_UP) {
+		if(pressed & KEY_UP && itemcount > 0) {
 			selection --;
 			//Sets selector to itemcount-1 (last place in 0 index array) if selector goes "above" top
 			if (selection < 0){
@@ -78,7 +106,7 @@ int main(int argc, char* argv[]) {
 			drawMenu(selection);
 		}
 		//Scroll down, wraps to top
-		if(pressed & KEY_DOWN) {
+		if(pressed & KEY_DOWN && itemcount > 0) {
 			selection ++;
 			//Sets selector to 0 (first place in 0 index array) if selector goes "below" bottom
 			if (selection == itemcount) {
@@ -87,9 +115,9 @@ int main(int argc, char* argv[]) {
 			drawMenu(selection);
 		}
 		//Displays select message when a button is pressed
-		if(pressed & KEY_A) {
+		if(pressed & KEY_A && itemcount > 0) {
 			drawMenu(selection);
-			iprintf("\x1b[10;2H ROM %s selected", items[selection]);
+			iprintf("\x1b[4;1H ROM %s selected", items[selection]);
 		}
 		if(pressed & KEY_START) break;
 	}
